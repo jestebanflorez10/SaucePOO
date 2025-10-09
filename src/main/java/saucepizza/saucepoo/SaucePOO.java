439400d9a228273225e17e50cecade24f44a8ede
jestebanflorez10 <jestebanflorez10@gmail.com>
2025-10-08 19:59:01 -0500

    Tercera Prueba

--- src/main/java/saucepizza/saucepoo/SaucePOO.java
+++ src/main/java/saucepizza/saucepoo/SaucePOO.java
@@ -6,6 +6,8 @@ public class SaucePOO {
     public static void main(String[] args) {
         System.out.println("Hola sauce!");
         System.out.println("sigo sin aprenderme los atajos...");
-        System.out.println("que se supone que hago aca?");
+        System.out.println("Apenas probar lo que estas editanto");
+        String dato = new String("Ok");
+        System.out.println(dato);
     }
 }
