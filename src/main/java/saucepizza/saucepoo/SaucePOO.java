5ef944cef98109fa855fcb3181e0bb5dbf45e3ca
jestebanflorez10 <jestebanflorez10@gmail.com>
2025-10-08 19:34:04 -0500

    Primera Prueba con GitHub Desktop for Windows
    ¡Hola Sauce!

--- src/main/java/saucepizza/saucepoo/SaucePOO.java
+++ src/main/java/saucepizza/saucepoo/SaucePOO.java
@@ -4,6 +4,6 @@ package saucepizza.saucepoo;
 public class SaucePOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola sauce!");
     }
 }
