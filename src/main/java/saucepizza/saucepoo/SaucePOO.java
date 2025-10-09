0bb8e89b5ff12685e0b13db8df3edac5aa796ad3
jestebanflorez10 <jestebanflorez10@gmail.com>
2025-10-08 20:20:09 -0500

    Integral los paquetes igu

--- src/main/java/saucepizza/saucepoo/SaucePOO.java
+++ src/main/java/saucepizza/saucepoo/SaucePOO.java
@@ -4,10 +4,6 @@ package saucepizza.saucepoo;
 public class SaucePOO {
 
     public static void main(String[] args) {
-        System.out.println("Hola sauce!");
-        System.out.println("sigo sin aprenderme los atajos...");
-        System.out.println("Apenas probar lo que estas editanto");
-        String dato = new String("ajamyap");
-        System.out.println(dato);
+        System.out.println("2025-II by Sauce Team");
     }
 }
